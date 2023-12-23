5224f0a1728a88647093bf4d36e3b2894ec14cbb
Gayal Seneviratne <gayals40@gmail.com>
2023-12-23 16:39:53 +0530

    Initialize Project

---
+++ src/test/java/com/gayal/recipe/RecipeShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayal.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
